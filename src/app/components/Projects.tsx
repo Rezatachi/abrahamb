790ba37242fec251d95c35d8a1def8b7a3a40041
Abraham Belayneh <abraham@caltech.edu>
2025-04-15 15:52:18 -0700

    v1.0.2

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -34,21 +34,23 @@ export default function Projects() {
     <section className="px-6 md:px-16 py-16 text-black min-h-screen md:h-screen" id="exp">
       <h2 className="text-xl font-semibold mb-12 text-[#386FA4] ml-27">&quot;EXPERIENCE&quot;</h2>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 px-6 md:px-16">
+      <div className="grid grid-cols-2 sm:grid-cols-2 gap-6 px-6 md:px-16">
         {experiences.reverse().map((exp, index) => (
           <div key={index} className="flex flex-col items-start text-left">
             {/* Boxed Logo */}
-            <div className="w-full h-80 flex items-center justify-center px-6 py-6 border border-white/20 hover:border-[#FFB703] transition duration-300 bg-black/10 p-4 md:p-6 rounded-lg shadow-lg relative group">
+            <div className="w-full h-80 flex items-center justify-center px-6 py-6 border border-white/20 bg-white p-4 md:p-6 rounded-lg shadow-lg relative group">
               <Image
                 src={exp.image}
                 alt={`${exp.company} logo`}
                 className="w-full h-full object-contain"
+                width={200}
+                height={200}
               />
             </div>
 
             {/* Text below */}
-            <div className="mt-4">
-              <p className="text-lg font-medium">{exp.title}</p>
+            <div className="mt-4 font-semibold">
+              <p className="text-lg font-semibold">{exp.title}</p>
               <p className="text-black/70">{exp.company}</p>
               <p className="text-black/40">{exp.year}</p>
             </div>
